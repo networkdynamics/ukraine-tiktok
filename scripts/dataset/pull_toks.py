de08af92300a3c67012029284233eb4ca4e0ba8a
Ben Steel <bendavidsteel@gmail.com>
2022-06-14 15:47:45 -0400

    Using custom verify fp

--- scripts/dataset/pull_toks.py
+++ scripts/dataset/pull_toks.py
@@ -7,7 +7,9 @@ from TikTokApi import TikTokApi
 def main(args):
     video_data = []
 
-    with TikTokApi() as api:
+    verify_fp = 'verify_l4ej0h1o_LxXoKmAk_T8sx_4sjK_9Naw_Iwn36mubUdc2'
+
+    with TikTokApi(custom_verify_fp=verify_fp) as api:
         for video in api.search.videos('ukraine'):
             video_data.append(video.info_full())
 
