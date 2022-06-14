8c19113fb51f1275db3a76db98e9725bfa4283e0
Ben Steel <bendavidsteel@gmail.com>
2022-06-14 16:40:09 +0000

    Fleshing out script

--- scripts/dataset/pull_toks.py
+++ scripts/dataset/pull_toks.py
@@ -1,4 +1,22 @@
+import argparse
+import json
+
 from TikTokApi import TikTokApi
 
-with TikTokApi() as api:
-    for 
+
+def main(args):
+    video_data = []
+
+    with TikTokApi() as api:
+        for video in api.search.videos('ukraine'):
+            video_data.append(video.info_full())
+
+    json.dump(video_data, args.out_path)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--out-path')
+    args = parser.parse_args()
+
+    main(args)
