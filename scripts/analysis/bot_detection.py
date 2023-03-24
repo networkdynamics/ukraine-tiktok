7c6d73bdbdaffa8ba9b8cf0ee66d6e3600af06d4
Ben Steel <bendavidsteel@gmail.com>
2023-03-24 19:20:05 +0000

    Added video sample filter

--- scripts/analysis/bot_detection.py
+++ scripts/analysis/bot_detection.py
@@ -1,14 +1,67 @@
-import botometer
+import json
+import os
+
+from birdspotter import BirdSpotter
+import pandas as pd
+
+from pytok import utils
 
 def main():
-    rapidapi_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    blt = botometer.BotometerLite(rapidapi_key=rapidapi_key)
+    this_dir_path = os.path.dirname(os.path.abspath(__file__))
+    data_dir_path = os.path.join(this_dir_path, '..', '..', 'data')
+    cache_dir_path = os.path.join(data_dir_path, 'cache')
 
-    # Prepare a list of tweets from the users that you want to perform bot detection on.
-    # The list should contain no more than 100 tweets.
-    tweet_list = [tweet1, tweet2, ...] 
+    # get user df
+    user_df_path = os.path.join(cache_dir_path, 'all_users.csv')
+    if os.path.exists(user_df_path):
+        user_df = pd.read_csv(user_df_path)
+    else:
+        comment_path = os.path.join(data_dir_path, 'comments')
+        comment_paths = [os.path.join(comment_path, dir_path, 'video_comments.json') for dir_path in os.listdir(comment_path)]
+        hashtag_dir_path = os.path.join(data_dir_path, 'hashtags')
+        searches_dir_path = os.path.join(data_dir_path, 'searches')
+        video_paths = [os.path.join(hashtag_dir_path, file_name) for file_name in os.listdir(hashtag_dir_path)] \
+                + [os.path.join(searches_dir_path, file_name) for file_name in os.listdir(searches_dir_path)]
+        # user_df = utils.get_user_df(user_df_path, file_paths=video_paths+comment_paths)
 
-    blt_scores = blt.check_accounts_from_tweets(tweet_list)
+    english_comments_path = os.path.join(cache_dir_path, 'all_english_comments.csv')
+    english_comments_df = pd.read_csv(english_comments_path)
+
+    # format comments for birdspotter
+    # english_comments_df = english_comments_df.merge(user_df, left_on='comment_author_id', right_on='user_id', how='left')
+
+    comments = []
+    for _, row in english_comments_df.iterrows():
+        comment = {}
+        comment['id_str'] = row['comment_id']
+        comment['created_at'] = row['createtime']
+        comment['text'] = row['text']
+        comment['user'] = {
+            'id_str': row['author_id'],
+            'name': row['author_name'],
+            'screen_name': row['author_name'],
+            'created_at': None, # row['user_createtime'],# TODO get from file
+            'description': None, # row['user_desc'],
+            'followers_count': None, #row['user_followers_count'],
+            'friends_count': None #row['user_following_count']
+        }
+        comment['entities'] = {
+            'hashtags': [],
+        }
+        comment['retweet_count'] = None
+        comment['favorite_count'] = None
+        comments.append(comment)
+
+    json_path = os.path.join(cache_dir_path, 'all_english_comments.jsonl')
+    with open(json_path, 'w') as f:
+        json.dump(comments, f, indent=4)
+
+    bs = BirdSpotter(json_path)
+    bot_scores = bs.getLabeledUsers()
+
+    # save to csv
+    bot_scores_path = os.path.join(cache_dir_path, 'all_english_comments_bot_scores.csv')
+    english_comments_df[['comment_id', 'bot_score']].to_csv(bot_scores_path)
 
 if __name__ == '__main__':
     main()
--- scripts/analysis/video_sample.py
+++ scripts/analysis/video_sample.py
@@ -1,20 +1,57 @@
+import datetime
 import os
 
+import ftlangdetect
+import numpy as np
 import pandas as pd
 
-import utils
+from pytok import utils
+
+def check_english(text):
+    try:
+        if text is np.nan:
+            return False
+        result = ftlangdetect.detect(text)
+        return result['lang'] == 'en'
+    except Exception as e:
+        if str(e) == 'No features in text.':
+            return False
+        else:
+            raise Exception('Unknown error')
 
 def main():
 
-    video_df = utils.get_video_df()
-    video_df = video_df.drop_duplicates('video_id')
-
-    NUM_SAMPLE = 100
-    video_sample = video_df.sample(NUM_SAMPLE)
-
     this_dir_path = os.path.dirname(os.path.abspath(__file__))
     data_dir_path = os.path.join(this_dir_path, '..', '..', 'data')
-    sample_path = os.path.join(data_dir_path, 'outputs', 'video_sample.csv')
+    video_path = os.path.join(data_dir_path, 'cache', 'all_videos.csv')
+    video_df = utils.get_video_df(video_path)
+
+    NUM_SAMPLE = 100
+
+    sample_type = 'english_and_invasion'
+    if sample_type == 'all':
+        video_sample = video_df.sample(NUM_SAMPLE)
+    elif sample_type == 'english_and_invasion':
+        comment_path = os.path.join(data_dir_path, 'cache', 'all_comments.csv')
+        comment_df = utils.get_comment_df(comment_path)
+        comment_df['english'] = comment_df['comment_language'] == 'en'
+        # get percentage of comments on each video
+        video_comments = comment_df[['video_id', 'comment_id', 'english']].groupby('video_id').agg({'english': 'sum', 'comment_id': 'count'})
+        video_comments['english_pct'] = video_comments['english'] / video_comments['comment_id']
+        
+        # join back to videos
+        video_sample = video_df.merge(video_comments, how='left', left_on='video_id', right_on='video_id')
+
+        # get vids with english descriptions
+        video_sample['english'] = video_sample['desc'].apply(check_english)
+
+        # get vids with at least 50% english comments or english description
+        video_sample = video_sample[(video_sample['english_pct'] > 0.5) | (video_sample['english'] == True)]
+        # get invasion videos
+        video_sample = video_sample[video_sample['createtime'].dt.year == 2022]
+        video_sample = video_sample.sample(NUM_SAMPLE)
+
+    sample_path = os.path.join(data_dir_path, 'outputs', f'{sample_type}_video_sample.csv')
     video_sample.to_csv(sample_path)
 
 if __name__ == '__main__':
